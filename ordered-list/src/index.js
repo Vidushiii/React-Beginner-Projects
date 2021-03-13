7c6c813b5173faf072230ee4413c8e7c1a042274
Vidushiii <vidutomar19@gmail.com>
2021-03-13 17:57:42 +0530

    first commit

---
+++ ordered-list/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <> <h1>Ordered List</h1>
+    <ol>
+      <li>Element 1</li>
+      <li>Element 2</li>
+      <li>Element 3</li>
+      <li>Element 4</li>
+      <li>Element 5</li>
+      <li>Element 6</li>
+    </ol>
+  </>, document.getElementById('root')
+);
+
